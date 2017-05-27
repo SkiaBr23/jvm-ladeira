072a1380e736ccb2cc258add4c430e349d4c78af
Silver472 <silver472@me.com>
2017-05-26 23:52:46 -0300

    Leitura para debug

--- leitura.c
+++ leitura.c
@@ -63,6 +63,7 @@ ClassFile* lerArquivo (char * nomeArquivo) {
 
 		cp_info *aux = NULL;
 
+		// Leitura do Constant Pool
 		for (aux=constantPool;aux<constantPool+arquivoClass->constant_pool_count-1;aux++){
 			aux->tag = u1Read(fp);
 			printf("TAG: %02x\n",aux->tag);
@@ -125,15 +126,15 @@ ClassFile* lerArquivo (char * nomeArquivo) {
 					for (u1 *i=aux->UnionCP.UTF8.bytes;i<aux->UnionCP.UTF8.bytes+aux->UnionCP.UTF8.length;i++){
 						*i = u1Read(fp);
 					}
-					printf("%04x\n",aux->UnionCP.UTF8.length);
+					printf("%02x\n",aux->UnionCP.UTF8.length);
 					for (u1 *i=aux->UnionCP.UTF8.bytes;i<aux->UnionCP.UTF8.bytes+aux->UnionCP.UTF8.length;i++){
-						printf("%04x\n",*i);
+						printf("%02x\n",*i);
 					}
 				break;
 				case CONSTANT_MethodHandle:
 					aux->UnionCP.MethodHandle.reference_kind = u1Read(fp);
 					aux->UnionCP.MethodHandle.reference_index = u2Read(fp);
-					printf("%04x\n",aux->UnionCP.MethodHandle.reference_kind);
+					printf("%02x\n",aux->UnionCP.MethodHandle.reference_kind);
 					printf("%04x\n",aux->UnionCP.MethodHandle.reference_index);
 				break;
 				case CONSTANT_MethodType:
@@ -149,8 +150,89 @@ ClassFile* lerArquivo (char * nomeArquivo) {
 				default:
 					printf("Default\n");
 				break;
-
 			}
+		}
+
+		arquivoClass->access_flags = u2Read(fp);
+		arquivoClass->this_class = u2Read(fp);
+		arquivoClass->super_class = u2Read(fp);
+		arquivoClass->interfaces_count = u2Read(fp);
+
+		printf("Access Flags: %04x\n",arquivoClass->access_flags);
+		printf("This Class: %04x\n",arquivoClass->this_class);
+		printf("Super Class: %04x\n",arquivoClass->super_class);
+		printf("Interfaces Count: %04x\n",arquivoClass->interfaces_count);
+
+		if(arquivoClass->interfaces_count>0){
+			// Preencher com leitura de interfaces
+			arquivoClass->interfaces = malloc(arquivoClass->interfaces_count*sizeof(u2));
+		}
+
+		arquivoClass->fields_count = u2Read(fp);
+		printf("Fields Count: %04x\n",arquivoClass->fields_count);
+
+		if(arquivoClass->fields_count>0){
+			// Preencher com leitura de fields
+			arquivoClass->fields = malloc(arquivoClass->fields_count*sizeof(u2));
+		}
+
+		arquivoClass->methods_count = u2Read(fp);
+		printf("Methods Count: %04x\n",arquivoClass->methods_count);
+
+		// Leitura de métodos
+		if(arquivoClass->methods_count>0){
+			arquivoClass->methods = malloc(arquivoClass->methods_count*sizeof(u2));
+
+			// Leitura da estrutura method_info, que é o vetor methods dentro do ClassFile
+			for (method_info *i=arquivoClass->methods;i<arquivoClass->methods+arquivoClass->methods_count;i++){
+				i->access_flags = u2Read(fp);
+				i->name_index = u2Read(fp);
+				i->descriptor_index = u2Read(fp);
+				i->attributes_count = u2Read(fp);
+
+				printf("Access Flags do método: %04x\n",i->access_flags);
+				printf("Name index do método: %04x\n",i->name_index);
+				printf("Descriptor Index do método: %04x\n",i->descriptor_index);
+				printf("Attributes Count do método: %04x\n",i->attributes_count);
+
+				// Leitura dos atributos de métodos
+				// Attributes count é o número de atributos
+				if(i->attributes_count>0){
+					i->attributes = malloc((i->attributes_count)*sizeof(attribute_info));
+					// Isso aqui é louco
+					// Leitura das strings
+					// Aqui lê cada atributo
+					for (attribute_info *a=i->attributes;a<(i->attributes)+(i->attributes_count);a++){
+						// Atribuir os dados do atributo
+						a->attribute_name_index = u2Read(fp);
+						printf("Attribute name index: %04x\n",a->attribute_name_index);
+						// Tamanho da string do atributo
+						a->attribute_length = u4Read(fp);
+						printf("Attribute length: %08x\n",a->attribute_length);
+						if(a->attribute_length>0){
+							a->info = malloc((a->attribute_length)*sizeof(u1));
+							// Tô loucasso
+							// Leitura dos caracteres da string pra armazenar em info
+							// Ler o info do atributo
+							for(u1 *c=(a->info);c<(a->info)+(a->attribute_length);c++){
+								*c = u1Read(fp);
+								printf("%02x\t",*c);
+							}
+
+							printf("\n");
+						}
+					}
+				}
+			}
+		}
+
+
+		// Leitura de atributos da classe
+		arquivoClass->attributes_count = u2Read(fp);
+		printf("Atributos da Classe: %02x\n",arquivoClass->attributes_count);
+
+		if(arquivoClass->attributes_count>0){
+			// Preencher com leitura de atributos
 		}
 
 		fclose(fp);
