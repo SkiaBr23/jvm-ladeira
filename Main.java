95f486388a1c28ee9da3dcf6b7a711d142c184c8
maximillianfx <maximillianfx@gmail.com>
2017-06-05 14:50:30 -0300

    Codigo para teste de double e long

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static final double varD = -15.8;
+    public static final long varL = 246;
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
