468bba012078ec7d984b7ce9c80f489b6bfecdf6
Silver472 <silver472@me.com>
2017-06-13 01:12:31 -0300

    Esse código não está compilando. A lógica básica de implementação de mais algumas instruções foi escrita.
    Vários ajustes ainda precisam ser feitos. Observar os comentários.

--- interpretador.c
+++ interpretador.c
@@ -1,106 +1,47 @@
 #include "interpretador.h"
-
-void iload_impl(u1 index, frame *f){
-	Push_operandos(f->p,(i4) *(f->v[index].variavel),INTEGER_OP);
-}
-
+#include <stdlib.h>
 
 void aconst_null_impl(frame *f){
 	Push_operandos(f->p,0,REFERENCE_OP); // 0 do tipo referência quer dizer referência apontando para NULL
 }
 
-void iconst_m1_impl(u1 constant, frame *f){
+void iconst_m1_impl(frame *f){
 	/* 
 		Tem um "shift" de -3. Ou seja, constant é um valor sem sinal.
 		Esse valor é subtraído de 3 para descobrir a constante que vai ser armazenada na pilha.
 	*/
-	i4 inteiro_sinal = (i4) constant-3;
+	i4 inteiro_sinal = (i4) -1;
 	Push_operandos(f->p,inteiro_sinal,INTEGER_OP);
 }
 
-void iconst_0_impl(u1 constant, frame *f){
-	i4 inteiro_sinal = (i4) constant-3;
+void iconst_0_impl(frame *f){
+	i4 inteiro_sinal = (i4) 0;
 	Push_operandos(f->p,inteiro_sinal,INTEGER_OP);
 }
 
-void iconst_1_impl(u1 constant, frame *f){
-	i4 inteiro_sinal = (i4) constant-3;
+void iconst_1_impl(frame *f){
+	i4 inteiro_sinal = (i4) 1;
 	Push_operandos(f->p,inteiro_sinal,INTEGER_OP);
 }
 
-void iconst_2_impl(u1 constant, frame *f){
-	i4 inteiro_sinal = (i4) constant-3;
+void iconst_2_impl(frame *f){
+	i4 inteiro_sinal = (i4) 2;
 	Push_operandos(f->p,inteiro_sinal,INTEGER_OP);
 }
 
-void iconst_3_impl(u1 constant, frame *f){
-	i4 inteiro_sinal = (i4) constant-3;
+void iconst_3_impl(frame *f){
+	i4 inteiro_sinal = (i4) 3;
 	Push_operandos(f->p,inteiro_sinal,INTEGER_OP);
 }
 
-void iconst_4_impl(u1 constant, frame *f){
-	i4 inteiro_sinal = (i4) constant-3;
+void iconst_4_impl(frame *f){
+	i4 inteiro_sinal = (i4) 4;
 	Push_operandos(f->p,inteiro_sinal,INTEGER_OP);
 }
 
-void iconst_5_impl(u1 constant, frame *f){
-	i4 inteiro_sinal = (i4) constant-3;
+void iconst_5_impl(frame *f){
+	i4 inteiro_sinal = (i4) 5;
 	Push_operandos(f->p,inteiro_sinal,INTEGER_OP);
-}
-
-/*
-	Long: pensar como faz
-*/
-void lconst_0_impl(u1 constant, frame *f){
-	Push_operandos(f->p,constant,LONG_OP);
-}
-
-/*
-	Long: pensar como faz
-*/
-void lconst_1_impl(u1 constant, frame *f){
-	Push_operandos(f->p,constant,LONG_OP);
-}
-
-
-/*
-	Float: pensar como faz.
-	A leitura é diferente, no formato IEEE754
-*/
-void fconst_0_impl(u1 constant, frame *f){
-	Push_operandos(f->p,constant,FLOAT_OP);
-}
-
-/*
-	Float: pensar como faz.
-	A leitura é diferente, no formato IEEE754
-*/
-void fconst_1_impl(u1 constant, frame *f){
-	Push_operandos(f->p,constant,FLOAT_OP);
-}
-
-/*
-	Float: pensar como faz.
-	A leitura é diferente, no formato IEEE754
-*/
-void fconst_2_impl(u1 constant, frame *f){
-	Push_operandos(f->p,constant,FLOAT_OP);
-}
-
-/*
-	Double: pensar como faz.
-	A leitura é diferente, no formato IEEE754
-*/
-void dconst_0_impl(u1 constant, frame *f){
-	Push_operandos(f->p,constant,DOUBLE_OP);
-}
-
-/*
-	Double: pensar como faz.
-	A leitura é diferente, no formato IEEE754
-*/
-void dconst_1_impl(u1 constant, frame *f){
-	Push_operandos(f->p,constant,DOUBLE_OP);
 }
 
 void bipush_impl(u1 byte, frame *f){
@@ -114,3 +55,134 @@ void sipush_impl(u1 byte1, u1 byte2, frame *f){
 	Push_operandos(f->p,byte_int,SHORT_OP);
 	Push_operandos(f->p,byte_int,SHORT_OP);
 }
+
+void iload_impl(u1 index, frame *f){
+	Push_operandos(f->p,(i4) *(f->v[index].variavel),INTEGER_OP);
+}
+
+void fload_impl(u1 index, frame *f){
+	Push_operandos(f->p, (i4) *(f->v[index].variavel),FLOAT_OP);
+}
+
+void aload_impl(u1 index, frame *f){
+	Push_operandos(f->p, (i4) *(f->v[index].variavel),REFERENCE_OP);
+}
+
+void iload_0_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[0].variavel),INTEGER_OP);
+}
+
+void iload_1_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[1].variavel),INTEGER_OP);
+}
+
+void iload_2_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[2].variavel),INTEGER_OP);
+}
+
+void iload_3_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[3].variavel),INTEGER_OP);
+}
+
+void fload_0_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[0].variavel),FLOAT_OP);
+}
+
+void fload_1_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[1].variavel),FLOAT_OP);
+}
+
+void fload_2_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[2].variavel),FLOAT_OP);
+}
+
+void fload_3_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[3].variavel),FLOAT_OP);
+}
+
+void aload_0_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[0].variavel),REFERENCE_OP);
+}
+
+void aload_1_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[1].variavel),REFERENCE_OP);
+}
+
+void aload_2_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[2].variavel),REFERENCE_OP);
+}
+
+void aload_3_impl(frame *f){
+	Push_operandos(f->p,(i4) *(f->v[3].variavel),REFERENCE_OP);
+}
+
+
+/* Verificar endereçamento */
+/* Vale para iaload a saload */
+void iaload_impl(frame *f){
+	pilha_operandos *indice = Pop_operandos(f->p);
+	pilha_operandos *referencia = Pop_operandos(f->p);
+
+	i4 *endereco = malloc(sizeof(i4 ));
+	endereco = referencia->topo->operando + indice->topo->operando;
+
+	// Objetivo: Acessar o conteúdo do endereço "referencia+indice"
+	// O código para esse acesso não parece correto, tem que analisar
+	Push_operandos(f->p,(i4) *endereco,INTEGER_OP);
+}
+
+void faload_impl(frame *f){
+	pilha_operandos *indice = Pop_operandos(f->p);
+	pilha_operandos *referencia = Pop_operandos(f->p);
+
+	// Acessar o conteúdo do endereço "referencia+indice"
+	// O código para esse acesso não parece correto, tem que analisar
+	Push_operandos(f->p,(i4) ,FLOAT_OP);
+}
+
+void aaload_impl(frame *f){
+	pilha_operandos *indice = Pop_operandos(f->p);
+	pilha_operandos *referencia = Pop_operandos(f->p);
+
+	Push_operandos(f->p,(i4) *((&(referencia->topo->operando))+(&(indice->topo->operando))),REFERENCE_OP);
+}
+
+void baload_impl(frame *f){
+	pilha_operandos *indice = Pop_operandos(f->p);
+	pilha_operandos *referencia = Pop_operandos(f->p);
+
+	Push_operandos(f->p,(i4) *(&(referencia->topo->operando)+&(indice->topo->operando)),BYTE_OP);
+}
+
+void caload_impl(frame *f){
+	pilha_operandos *indice = Pop_operandos(f->p);
+	pilha_operandos *referencia = Pop_operandos(f->p);
+
+	Push_operandos(f->p,(i4) *(&(referencia->topo->operando)+&(indice->topo->operando)),CHAR_OP);
+}
+
+void saload_impl(frame *f){
+	pilha_operandos *indice = Pop_operandos(f->p);
+	pilha_operandos *referencia = Pop_operandos(f->p);
+
+	Push_operandos(f->p,(i4) *(&(referencia->topo->operando)+&(indice->topo->operando)),SHORT_OP);
+}
+
+void istore_impl(u1 index,frame *f){
+	pilha_operandos *valor = Pop_operandos(f->p);
+
+	*(f->v[index].variavel) = (u4) valor->topo->operando;
+}
+
+void fstore_impl(u1 index, frame *f){
+	pilha_operandos *valor = Pop_operandos(f->p);
+	*(f->v[index].variavel) = (u4) valor->topo->operando;
+}
+
+
+
+
+
+
+
+
--- interpretador.h
+++ interpretador.h
@@ -3,23 +3,64 @@
 
 #include "instrucoes.h"
 
-void iload_impl(u1 index, frame *f);
+/* Ficamos na dúvida entre u1 e u4, não sabemos qual valor deve ser mandado para o argumento das funções */
+void nop_impl(void); // Não implementado
 void aconst_null_impl(frame *f);
-void iconst_m1_impl(u1 constant, frame *f);
-void iconst_0_impl(u1 constant, frame *f);
-void iconst_1_impl(u1 constant, frame *f);
-void iconst_2_impl(u1 constant, frame *f);
-void iconst_3_impl(u1 constant, frame *f);
-void iconst_4_impl(u1 constant, frame *f);
-void iconst_5_impl(u1 constant, frame *f);
-void lconst_0_impl(u1 constant, frame *f);
-void lconst_1_impl(u1 constant, frame *f);
-void fconst_0_impl(u1 constant, frame *f);
-void fconst_1_impl(u1 constant, frame *f);
-void fconst_2_impl(u1 constant, frame *f);
-void dconst_0_impl(u1 constant, frame *f);
-void dconst_1_impl(u1 constant, frame *f);
+void iconst_m1_impl(frame *f);
+void iconst_0_impl(frame *f);
+void iconst_1_impl(frame *f);
+void iconst_2_impl(frame *f);
+void iconst_3_impl(frame *f);
+void iconst_4_impl(frame *f);
+void iconst_5_impl(frame *f);
+void lconst_0_impl(frame *f); // Não implementado
+void lconst_1_impl(frame *f); // Não implementado
+void fconst_0_impl(frame *f); // Não implementado
+void fconst_1_impl(frame *f); // Não implementado
+void fconst_2_impl(frame *f); // Não implementado
+void dconst_0_impl(frame *f); // Não implementado
+void dconst_1_impl(frame *f); // Não implementado
 void bipush_impl(u1 byte, frame *f);
 void sipush_impl(u1 byte1, u1 byte2, frame *f);
+void ldc_impl(void); // Não implementado
+void ldc_w_impl(void); // Não implementado
+void ldc2_w_impl(void); // Não implementado
+void iload_impl(u1 index, frame *f);
+void lload_impl(u1 index, frame *f); // Não implementado
+void fload_impl(u1 index, frame *f);
+void dload_impl(void); // Não implementado
+void aload_impl(u1 index, frame *f);
+void iload_0_impl(frame *f);
+void iload_1_impl(frame *f);
+void iload_2_impl(frame *f);
+void iload_3_impl(frame *f);
+void lload_0_impl(frame *f); // Não implementado
+void lload_1_impl(frame *f); // Não implementado
+void lload_2_impl(frame *f); // Não implementado
+void lload_3_impl(frame *f); // Não implementado
+void fload_0_impl(frame *f);
+void fload_1_impl(frame *f);
+void fload_2_impl(frame *f);
+void fload_3_impl(frame *f);
+void dload_0_impl(frame *f); // Não implementado
+void dload_1_impl(frame *f); // Não implementado
+void dload_2_impl(frame *f); // Não implementado
+void dload_3_impl(frame *f); // Não implementado
+void aload_0_impl(frame *f);
+void aload_1_impl(frame *f);
+void aload_2_impl(frame *f);
+void aload_3_impl(frame *f);
+/* Verificar iaload até saload, tem que ajustar a lógica de acesso ao array no índice x. */
+void iaload_impl(frame *f);
+void laload_impl(frame *f); // Não implementado
+void faload_impl(frame *f);
+void daload_impl(frame *f); // Não implementado
+void aaload_impl(frame *f);
+void baload_impl(frame *f);
+void caload_impl(frame *f);
+void saload_impl(frame *f);
+void istore_impl(u1 index, frame *f);
+void lstore_impl(u1 index, frame *f); // Não implementado
+void fstore_impl(u1 index, frame *f);
 
 #endif
